b0420d090b3bb1829376f8a6d37abe999ca7badc
Lily Riopelle <lilyriopelle@Lilys-MBP-2.home>
2015-05-14 00:04:40 -0700

    Add show route to Api::MenusController

--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,6 +1,8 @@
-class Api::MenusController
+class Api::MenusController < ApplicationController
 
   def show
+    @menu = Menu.find(params[:id])
+    render :show
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api , defaults: { format: :json } do
     resources :recipes
+    resources :menus
     resources :users, only: [:show]
   end
 
