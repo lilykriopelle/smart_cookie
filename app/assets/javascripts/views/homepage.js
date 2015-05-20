e06009715678c2e50afa2eefaf4600bfb2bccf26
Lily Riopelle <lilyriopelle@192.168.1.87>
2015-05-20 09:47:42 -0400

    refactor homepage filtering

--- app/assets/javascripts/views/homepage.js
+++ app/assets/javascripts/views/homepage.js
@@ -9,47 +9,24 @@ CookingGenius.Views.Homepage = Backbone.CompositeView.extend({
   },
 
   initialize: function() {
-    var allRecipes =  new CookingGenius.Collections.Recipes();
-    allRecipes.fetch({
+    this.displayFilteredRecpies();
+  },
+
+  displayFilteredRecpies: function(event) {
+    var tag = event ? $(event.currentTarget).text(): "";
+    this.$(".recipe-feed-container").empty();
+    filtered = new CookingGenius.Collections.Recipes();
+    filtered.fetch({
+      data: { primary_tag: tag == "all" ? "" : tag},
       success: function() {
         var recipeFeed = new CookingGenius.Views.RecipeFeed({
-          collection: allRecipes,
-          primary_tag: "all"
+          collection: filtered,
+          primary_tag: tag
         });
         this.addSubview(".recipe-feed-container", recipeFeed);
       }.bind(this)
     });
   },
-
-  displayFilteredRecpies: function(event) {
-    var tag = $(event.currentTarget).text();
-    this.$(".recipe-feed-container").empty();
-    filtered = new CookingGenius.Collections.Recipes();
-
-    if (tag != "all") {
-      filtered.fetch({
-        data: {primary_tag: tag},
-        success: function() {
-          var recipeFeed = new CookingGenius.Views.RecipeFeed({
-            collection: filtered,
-            primary_tag: tag
-          });
-          this.addSubview(".recipe-feed-container", recipeFeed);
-        }.bind(this)
-      });
-    } else {
-      filtered.fetch({
-        success: function() {
-          var recipeFeed = new CookingGenius.Views.RecipeFeed({
-            collection: filtered,
-            primary_tag: tag
-          });
-          this.addSubview(".recipe-feed-container", recipeFeed);
-        }.bind(this)
-      });
-    }
-  },
-
 
   render: function() {
     this.$el.html(this.template());
