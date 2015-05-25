b0160dc748d9a3dc5d2cd7bb778fb2a2d4a2af4b
Lily Riopelle <lilyriopelle@192.168.1.87>
2015-05-25 12:53:57 -0400

    Fix users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521150825) do
+ActiveRecord::Schema.define(version: 20150525165119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,9 +116,12 @@ ActiveRecord::Schema.define(version: 20150521150825) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "name",            null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
   add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
 
   create_table "votes", force: :cascade do |t|
