e4dc26c132dd0f772b6d436f71c7e392804d48c5
Lily Riopelle <lilyriopelle@Lilys-MacBook-Pro-2.local>
2015-05-29 23:38:38 -0400

    fix filter and sort by order

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -3,10 +3,11 @@ class Api::RecipesController < ApplicationController
   def index
     @recipes = Recipe.includes(:votes)
                      .where(nil)
-                     .sort { |a,b| b.votes().size <=> a.votes().size}
     filters.each do |k, v|
       @recipes = @recipes.send(k, v) if v.present?
     end
+
+    @recipes.sort { |a,b| b.votes().size <=> a.votes().size}
 
     if (params[:page])
       @recipes = Kaminari.paginate_array(@recipes)
