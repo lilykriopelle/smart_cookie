f1ca5d7d5d2834905e1b2b36b72e78f00faa23aa
Lily Riopelle <lilyriopelle@192.168.1.87>
2015-06-01 17:10:47 -0400

    include more associations in recipe controller

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -18,7 +18,7 @@ class Api::RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.includes(:author, :ingredients, :votes, annotations: [:author, replies: :author]).find(params[:id])
+    @recipe = Recipe.includes(:author, :votes, :ingredients, recipes_ingredients: [annotations: [:author, replies: :author]], annotations: [:author, replies: :author]).find(params[:id])
     render :show
   end
 
